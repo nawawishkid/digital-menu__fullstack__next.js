cedd0467317b9ec14f619e062df35c7601140234
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-08 20:00:43 +0700

    fix: handle when user id in cookies exists but user not found with the id

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -5,9 +5,16 @@ export default () => async (req, res, next) => {
   const cookies = makeCookies(req, res);
   const userId = cookies.get("u", { signed: true });
 
+  console.log(`userId: `, userId);
   if (!userId) return res.status(401).end();
 
   const [user] = await findUserById(userId);
+
+  if (!user)
+    return next({
+      statusCode: 401,
+      data: { message: `User id '${userId}' is not found` },
+    });
 
   req.user = user;
 
