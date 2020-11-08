7eb2e2687959563c241f15ec39b696c2802055e0
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-08 20:39:32 +0700

    refactor: add files service

---
+++ services/files.js
@@ -0,0 +1,20 @@
+import nanoid from "../helpers/nanoid";
+import knex from "../knex";
+
+export const findFiles = (select = null) => {
+  return knex("files").select(select);
+};
+
+export const findFileById = id => knex("files").select().where("id", id);
+export const findFileByOwnerId = ownerId =>
+  knex("files").select().where("owner", ownerId);
+export const createFile = data => {
+  const id = nanoid();
+
+  return knex("files")
+    .insert({ ...data, id })
+    .then(() => id);
+};
+export const deleteFileById = id => knex("files").where("id", id).delete();
+export const updateFileById = (id, data) =>
+  knex("files").where("id", id).update(data);
