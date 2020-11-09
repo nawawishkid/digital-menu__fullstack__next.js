6a379ec719524f1b762c076e08d1c287b01099e0
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-09 14:27:34 +0700

    fix: wait for router.query.menuId to load before rendering the form

--- pages/[restaurantId]/menus/[menuId]/new-dish.js
+++ pages/[restaurantId]/menus/[menuId]/new-dish.js
@@ -14,13 +14,38 @@ const validationSchema = yup.object().shape({
   pictures: yup.array(yup.mixed()).required(),
   cuisine: yup.number(),
   ingredients: yup.array(yup.number()),
-  menu: yup.string().uuid().required(),
+  menu: yup
+    .string()
+    .required()
+    .matches(/^[a-zA-Z0-9]{8}$/),
 });
+const initialValues = {
+  name: "",
+  price: "",
+  description: "",
+  pictures: [],
+  cuisine: "",
+  ingredients: [],
+  menu: undefined,
+};
 
 export default function NewDish() {
   const router = useRouter();
   const [cuisines, setCuisines] = React.useState(null);
   const [previewPictures, setPreviewPictures] = React.useState(null);
+
+  React.useEffect(() => {
+    Axios.get("/api/cuisines")
+      .then(res => setCuisines(res.data.cuisines))
+      .catch(console.log);
+  }, []);
+
+  const { menuId } = router.query;
+
+  if (!menuId) return <p>Loading...</p>;
+
+  initialValues.menu = menuId;
+
   const readFile = file => {
     const fr = new FileReader();
 
@@ -41,118 +66,102 @@ export default function NewDish() {
     Object.values(target.files).forEach(readFile);
   };
 
-  React.useEffect(() => {
-    Axios.get("/api/cuisines")
-      .then(res => setCuisines(res.data.cuisines))
-      .catch(console.log);
-  }, []);
-
   console.log(`cuisines: `, cuisines);
   console.log(`previewPictures: `, previewPictures);
+  console.log(`menuId: `, router.query.menuId);
+  console.log(`initialValues: `, initialValues);
 
   return (
     <Formik
-      initialValues={{
-        name: "",
-        price: "",
-        description: "",
-        pictures: [],
-        cuisine: "",
-        ingredients: [],
-        menu: router.query.id,
-      }}
+      initialValues={initialValues}
       validationSchema={validationSchema}
-      onSubmit={(values, actions) => {
-        console.log(`values: `, values);
-        actions.setSubmitting(false);
+      onSubmit={values => {
+        console.log(`submitted values: `, values);
       }}
     >
-      {({ isSubmitting, setFieldValue, values }) =>
-        console.log(`values: `, values) || (
-          <Form>
-            <Field type="hidden" name="menu" />
-            <div>
-              {previewPictures && previewPictures.length ? (
-                <PicturesSlider pictures={previewPictures} />
-              ) : (
-                <label
-                  htmlFor="pictures"
-                  className="w-full flex justify-center items-center bg-gray-200 text-gray-500"
-                  style={{ height: `240px` }}
-                >
-                  Tap here to add picture
-                </label>
-              )}
-              <BaseField name="pictures">
-                <input
-                  type="file"
-                  name="pictures"
-                  id="pictures"
-                  accept="image/*"
-                  multiple
-                  className="hidden"
-                  onChange={e => {
-                    filesToPriviewPictures(e);
-                    setFieldValue("pictures", Object.values(e.target.files));
-                  }}
-                />
-              </BaseField>
-            </div>
-            <div className="p-4">
-              <BaseField name="name">
-                <h1>
-                  <FormikField
-                    type="text"
-                    name="name"
-                    placeholder="Name here"
-                    className="w-full"
-                  />
-                </h1>
-              </BaseField>
-              <BaseField name="price">
-                <h2>
-                  ฿{" "}
-                  <FormikField
-                    type="number"
-                    name="price"
-                    placeholder="Price here"
-                  />
-                </h2>
-              </BaseField>
-              <Field
-                name="description"
-                component="textarea"
-                placeholder="Description here..."
-                className="w-full"
+      {({ isSubmitting, setFieldValue }) => (
+        <Form>
+          <div>
+            {previewPictures && previewPictures.length ? (
+              <PicturesSlider pictures={previewPictures} />
+            ) : (
+              <label
+                htmlFor="pictures"
+                className="w-full flex justify-center items-center bg-gray-200 text-gray-500"
+                style={{ height: `240px` }}
+              >
+                Tap here to add picture
+              </label>
+            )}
+            <BaseField name="pictures">
+              <input
+                type="file"
+                name="pictures"
+                id="pictures"
+                accept="image/*"
+                multiple
+                className="hidden"
+                onChange={e => {
+                  filesToPriviewPictures(e);
+                  setFieldValue("pictures", Object.values(e.target.files));
+                }}
               />
-              <div>
-                <p className="font-bold mb-2">Cuisine</p>
-                <Field name="cuisine" component="select">
-                  {cuisines === null
-                    ? `Loading...`
-                    : cuisines.map(cuisine => (
-                        <option value={cuisine.id}>{cuisine.name}</option>
-                      ))}
-                </Field>
-              </div>
-              <div>
-                <p className="font-bold mb-2">Ingredients</p>
-                <Field
+            </BaseField>
+          </div>
+          <div className="p-4">
+            <BaseField name="name">
+              <h1>
+                <FormikField
                   type="text"
-                  name="ingredients"
-                  placeholder="Type your ingredient and type enter"
+                  name="name"
+                  placeholder="Name here"
                   className="w-full"
                 />
-              </div>
-              <div className="flex justify-end mt-8">
-                <Button type="submit" disabled={isSubmitting}>
-                  Save
-                </Button>
-              </div>
+              </h1>
+            </BaseField>
+            <BaseField name="price">
+              <h2>
+                ฿{" "}
+                <FormikField
+                  type="number"
+                  name="price"
+                  placeholder="Price here"
+                />
+              </h2>
+            </BaseField>
+            <Field
+              name="description"
+              component="textarea"
+              placeholder="Description here..."
+              className="w-full"
+            />
+            <div>
+              <p className="font-bold mb-2">Cuisine</p>
+              <Field name="cuisine" component="select">
+                {cuisines === null
+                  ? `Loading...`
+                  : cuisines.map(cuisine => (
+                      <option value={cuisine.id}>{cuisine.name}</option>
+                    ))}
+              </Field>
             </div>
-          </Form>
-        )
-      }
+            <div>
+              <p className="font-bold mb-2">Ingredients</p>
+              <Field
+                type="text"
+                name="ingredients"
+                placeholder="Type your ingredient and type enter"
+                className="w-full"
+              />
+            </div>
+            <div className="flex justify-end mt-8">
+              <Button type="submit" disabled={isSubmitting}>
+                Save
+              </Button>
+            </div>
+          </div>
+        </Form>
+      )}
     </Formik>
   );
 }
