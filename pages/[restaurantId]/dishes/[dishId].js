914c9f9962c3a203aaa8c7f474007f85e8bb24fd
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-12 18:34:36 +0700

    feat: add dish profile page (basic)

---
+++ pages/[restaurantId]/dishes/[dishId].js
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default function DishProfile({ dish }) {
+  return dish ? (
+    <div>
+      <div className="mb-4">
+        {dish.pictures.map((pic, index) => (
+          <img src={pic} key={index} />
+        ))}
+      </div>
+      <div className="mb-4">
+        <h1 className="mb-2">{dish.name}</h1>
+        <h2 className="mb-4">฿{dish.price}</h2>
+        <p>{dish.description}</p>
+      </div>
+      {dish.cuisine ? (
+        <div>
+          <p className="bold">Cuisine</p>
+          <p>{dish.cuisine}</p>
+        </div>
+      ) : null}
+      {dish.ingredients ? (
+        <div>
+          <p className="bold">Ingredients</p>
+          <div>{dish.ingredients.map(ingredient => null)}</div>
+        </div>
+      ) : null}
+    </div>
+  ) : (
+    <p>Dish not found</p>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const getDishesServiceInstance = require("../../../helpers/get-dishes-service-instance")
+    .default;
+  const dishesService = getDishesServiceInstance();
+  const { dishId } = params;
+  let dish;
+
+  if (!dishId) {
+    dish = null;
+  } else {
+    const foundDish = await dishesService.findDishById(dishId);
+
+    dish = !foundDish ? null : foundDish;
+  }
+
+  return { props: { dish } };
+}
