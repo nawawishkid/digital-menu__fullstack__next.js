8de0fc9445e95b430e891e0dcb6dab220f3aebca
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-12 19:27:11 +0700

    fix: add padding

--- pages/[restaurantId]/dishes/[dishId].js
+++ pages/[restaurantId]/dishes/[dishId].js
@@ -3,28 +3,30 @@ import React from "react";
 export default function DishProfile({ dish }) {
   return dish ? (
     <div>
-      <div className="mb-4">
+      <div>
         {dish.pictures.map((pic, index) => (
           <img src={pic} key={index} />
         ))}
       </div>
-      <div className="mb-4">
-        <h1 className="mb-2">{dish.name}</h1>
-        <h2 className="mb-4">฿{dish.price}</h2>
-        <p>{dish.description}</p>
+      <div className="p-4">
+        <div className="mb-4">
+          <h1 className="mb-2">{dish.name}</h1>
+          <h2 className="mb-4">฿{dish.price}</h2>
+          <p>{dish.description}</p>
+        </div>
+        {dish.cuisine ? (
+          <div>
+            <p className="bold">Cuisine</p>
+            <p>{dish.cuisine}</p>
+          </div>
+        ) : null}
+        {dish.ingredients ? (
+          <div>
+            <p className="bold">Ingredients</p>
+            <div>{dish.ingredients.map(ingredient => null)}</div>
+          </div>
+        ) : null}
       </div>
-      {dish.cuisine ? (
-        <div>
-          <p className="bold">Cuisine</p>
-          <p>{dish.cuisine}</p>
-        </div>
-      ) : null}
-      {dish.ingredients ? (
-        <div>
-          <p className="bold">Ingredients</p>
-          <div>{dish.ingredients.map(ingredient => null)}</div>
-        </div>
-      ) : null}
     </div>
   ) : (
     <p>Dish not found</p>
