780ab911c5f5e8959c3db83077abbff37559e418
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-14 16:04:48 +0700

    fix: add search & filter icons

--- components/dish-list.js
+++ components/dish-list.js
@@ -1,3 +1,6 @@
+import { Icon } from "@iconify/react";
+import searchIcon from "@iconify/icons-ei/search";
+import filterIcon from "@iconify/icons-bi/filter";
 import useMenu from "../hooks/use-menu";
 import Button from "./button";
 import DishCard from "./dish-card";
@@ -9,6 +12,20 @@ export default function DishList({ dishes, restaurantId }) {
 
   return dishes.length ? (
     <div>
+      <div className="flex justify-end items-center">
+        <Icon
+          icon={searchIcon}
+          width="2em"
+          height="2em"
+          className="text-gray-700"
+        />
+        <Icon
+          icon={filterIcon}
+          width="2em"
+          height="2em"
+          className="text-gray-700 ml-2"
+        />
+      </div>
       <div className="flex flex-wrap justify-between mb-8">
         {dishes.map(dish => (
           <DishCard {...dish} key={dish.id} restaurantId={restaurantId} />
