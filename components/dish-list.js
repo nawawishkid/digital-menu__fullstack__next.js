aca37e270a4fe808354c76faf9954c56ca81f567
nawawishkid <nawawish.samerpark@gmail.com>
2020-11-14 14:22:14 +0700

    fix: add create-another-dish button

--- components/dish-list.js
+++ components/dish-list.js
@@ -1,12 +1,26 @@
+import useMenu from "../hooks/use-menu";
+import Button from "./button";
 import DishCard from "./dish-card";
+import Link from "./link";
 import NoDish from "./no-dish";
 
 export default function DishList({ dishes, restaurantId }) {
+  const [menu] = useMenu(restaurantId);
+
   return dishes.length ? (
-    <div className="flex flex-wrap justify-between">
-      {dishes.map(dish => (
-        <DishCard {...dish} key={dish.id} restaurantId={restaurantId} />
-      ))}
+    <div>
+      <div className="flex flex-wrap justify-between mb-8">
+        {dishes.map(dish => (
+          <DishCard {...dish} key={dish.id} restaurantId={restaurantId} />
+        ))}
+      </div>
+      {menu ? (
+        <Link href={`/@${restaurantId}/menus/${menu.id}/new-dish`}>
+          <Button>Create another dish</Button>
+        </Link>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   ) : (
     <NoDish restaurantId={restaurant.id} />
